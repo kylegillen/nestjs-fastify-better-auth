5818830d289fafec8bb06c682a12fa0fb473dfbe
Kyle G <kyle@nextriot.com>
2025-07-02 19:30:12 -0300

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  type: 'lib',
+  typescript: true,
+  lessOpinionated: true,
+  stylistic: {
+    quotes: 'single',
+    semi: false,
+  },
+})
